31e82b2fa5132881e45506fdea3520d2e09e01e8
Victor Szczepanski <acgs02@gmail.com>
2014-11-09 17:54:17 -0700

    Moved configuration and serial helper functions into basic.ino. Made configuration variables (including serial) global.

--- firmware/basic/basic.ino
+++ firmware/basic/basic.ino
@@ -14,15 +14,174 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>
 */
+#include "Arduino.h"
+#include <SoftwareSerial.h>
+#define FS(x) (__FlashStringHelper*)(x)
 
-#include <SerialHelper.h>
-#include <Config.h>
 
+/*
+--------------------------------------------------------------------------
+Configuration
+--------------------------------------------------------------------------
+*/
+
+/* Serial config */
+#define USESOFTSERIAL 0
+
+//Values below are irrelevent if softserial is not used.
+#if USESOFTSERIAL
+
+#define SOFTSERIALRX 6
+#define SOFTSERIALTX 7
+
+#endif
+
+
+/*Sensor pin config. May use Arduino macros. Value of 0 indicates no pin -
+this is useful for e.g. 'soft' sensors that collect data not from a pin. See
+RMSValue on MicSensor.*/
+#define OUTPUTSENSORA A0
+#define OUTPUTSENSORB A1
+#define OUTPUTNAMEA "Sensor1"
+#define OUTPUTNAMEB "Sensor2"
+
+#define INPUTSENSORA 13
+#define INPUTNAMEA "LED1"
+
+//Required defines. Use 0 if no sensors of that type are present.
+#define NUMOUTPUTSENSORS 2
+#define NUMINPUTSENSORS 1
+
+//Do not change
+#if USESOFTSERIAL
+SoftwareSerial myserial(SOFTSERIALRX,SOFTSERIALTX);
+#define SERIAL myserial
+#else
+#define SERIAL Serial
+#endif
+
+
+/*
+--------------------------------------------------------------------------
+Variable declarations
+
+Variables:
+const char jsonData[] PROGMEM
+TELECEPTORSERIAL serial
+int outputsensorpins[]
+int outputsensorvalues[]
+char * outputsensornames[]
+int inputsensorpins[]
+boolean inputsensorstate[]
+char * inputsensornames[]
+--------------------------------------------------------------------------
+*/
+
+//Use Flash memory to save Ram
+const char jsonData[] PROGMEM = "{\"model\": \"001-TEMP0002\",\"description\": \"TEMPMIC Sensor A\", \"in\": [], \"uuid\": \"TEMPMIC\", \"out\": [{\"name\": \"temp1\", \"description\": \"Temp Sensor\",\"units\":\"degrees F\",\"model\":\"m\",\"sensor_type\":\"float\", \"scale\": [0.18,32], \"timestamp\" : 1404436932.2},{\"name\": \"temp2\", \"description\": \"Temp Sensor\",\"units\":\"degrees F\",\"model\":\"m\",\"sensor_type\":\"float\", \"scale\": [0.18,32], \"timestamp\" : 1404436932.2},{\"name\": \"RMSValue\", \"description\": \"Temp Sensor\",\"units\":\"degrees F\",\"model\":\"m\",\"sensor_type\":\"float\", \"scale\": [1,0], \"timestamp\" : 1404436932.2},{\"name\": \"RMSCurrent\", \"description\": \"Temp Sensor\",\"units\":\"degrees F\",\"model\":\"m\",\"sensor_type\":\"float\", \"scale\": [1,0], \"timestamp\" : 1404436932.2}]}";
+
+//modify the array defs based on the defined sensors above.
+int outputsensorpins[] = {OUTPUTSENSORA, OUTPUTSENSORB};
+int outputsensorvalues[] = {0, 0};
+char * outputsensornames[] = {OUTPUTNAMEA, OUTPUTNAMEB};
+
+int inputsensorpins[] = {INPUTSENSORA};
+boolean inputsensorstate[] = {true};
+char * inputsensornames[] = {INPUTNAMEA};
+
+
+
+/*
+--------------------------------------------------------------------------
+Helper Functions
+
+Functions:
+void writeSensorStates()
+void serialComm()
+--------------------------------------------------------------------------
+*/
+/**
+Convience function to write out the values of each sensor in the nested array format [ ["sensorname", sensorvalue], ["sensorname", sensorvalue], ... ]
+*/
+void writeSensorStates(){
+    int numsensors = NUMOUTPUTSENSORS + NUMINPUTSENSORS;
+    SERIAL.print("[");
+    for(int i = 0; i < numsensors; i++){
+        SERIAL.print("[");
+        if(i < NUMOUTPUTSENSORS){
+            SERIAL.print('"');
+            SERIAL.print(outputsensornames[i]);
+            SERIAL.print('"');
+            SERIAL.print(",");
+            SERIAL.print(outputsensorvalues[i]);
+        }
+        else{
+            SERIAL.print('"');
+            SERIAL.print(inputsensornames[i-NUMOUTPUTSENSORS]);
+            SERIAL.print('"');
+            SERIAL.print(",");
+            SERIAL.print(inputsensorstate[i-NUMOUTPUTSENSORS]);
+        }
+        SERIAL.print("]");
+        if(i < numsensors-1) SERIAL.print(",");
+    }
+    SERIAL.println("]");
+}
+
+
+/**
+Param: Stream s - object of type Stream. Stream inherits from Print, so we still have access to print statements.
+
+serialComm will read in serial data as described in the file basestationHOWTO (TODO: Update basestationHOWTO with current serial protocol). It will write out sensor states upon receiving a '%' symbol, and will interpret a flat JSON object upon receiving a '@' symbol. Input sensor states will be changed accordingly.
+
+See Also: writeSensorStates
+*/
+void serialComm(){
+    if(SERIAL.available() > 0){
+        char serialRead = SERIAL.read();
+
+        if(serialRead == '@'){
+            while(!SERIAL.available()){} //wait for more data
+            if (SERIAL.available()) {
+                /* First, skip any accidental whitespace like newlines. */
+                char whitespace = SERIAL.peek();
+                while(whitespace == ' ' || whitespace == '\t' || whitespace == '\n'){SERIAL.read(); whitespace = SERIAL.peek();}
+            }
+
+            if (SERIAL.available()) {
+                //replace this section with new JSON parser.
+                /*
+                aJsonObject *msg = aJson.parse(&serial_stream, jsonFilter);
+                processMessage(msg);
+                aJson.deleteItem(msg);
+                */
+            }
+        }
+
+        if(serialRead == '%'){
+            SERIAL.println(FS(jsonData));
+
+            writeSensorStates();
+       }
+    }
+}
+
+
+
+/*
+--------------------------------------------------------------------------
+Main Program code
+
+Functions:
+void setup()
+void loop()
+--------------------------------------------------------------------------
+*/
 
 // the setup routine runs once when you press reset:
 void setup() {
     // initialize serial communication at 9600 bits per second:
-    serial.begin(9600);
+    SERIAL.begin(9600);
 
     //initialize all input pins as OUTPUT and set HIGH
     for(int i = 0; i < NUMINPUTSENSORS; i++){
@@ -50,7 +209,7 @@ void loop() {
 
 
     //check for data on serial
-    serialComm(serial);
+    serialComm();
 
 
 }
