89a33db2f2f70ff0eb9471543e140d8a37bd9855
Victor Szczepanski <acgs02@gmail.com>
2015-01-26 18:38:13 -0700

    Created superclass for test files to inherit from. Moved tests to new folder unittests. Renamed test.py to functionalTest.py.

---
+++ unittests/abstractTest.py
@@ -0,0 +1,96 @@
+"""
+abstractTest.py
+
+Contributing Authors:
+Evan Salazar (Visgence, Inc.)
+Victor Szczepanski (Visgence, Inc.)
+
+This module defines the setup and teardown methods that all teleceptor test suites can inherit from.
+
+Depencencies:
+unittest
+
+"""
+
+#!/usr/bin/env python
+import unittest
+import os
+import sys
+import subprocess
+import time
+import json
+import requests
+import tempfile
+import shutil
+PATH = os.path.abspath(os.path.join(os.path.dirname(os.path.dirname(__file__))))
+sys.path.append(PATH)
+
+
+DATAPATH = tempfile.mkdtemp(prefix="tmpteleceptor")
+TEST_PORT = 20000
+URL = "http://localhost:%d/" % (TEST_PORT)
+print URL
+print DATAPATH
+
+def build_config():
+
+    data = {"DBFILE": "database/base_station.db",
+            "WHISPER_DATA": "whisperData",
+            "WHISPER_ARCHIVES": ["60:1440", "15m:14d", "6h:1y"],
+            "SQLDATA": True,
+            "SQLREADTIME": 7200,
+            "PORT": TEST_PORT,
+            "LOG": "teleceptor.log",
+            "TCP_POLLER_HOSTS": [],
+            "USE_DEBUG": False,
+            "SUPRESS_SERVER_OUTPUT": True}
+
+    conf = open(os.path.join(DATAPATH, 'config.json'), "w")
+    json.dump(data,conf,indent=4)
+    conf.close()
+
+
+class AbstractTeleceptorTest(unittest.TestCase):
+
+    process = None
+
+    @classmethod
+    def setUpClass(self):
+        build_config()
+        os.environ["TELECEPTOR_DATAPATH"] = DATAPATH
+        #Set up the database and create config information
+        assert subprocess.call([sys.executable, os.path.join(PATH, 'teleceptorcmd'), 'setup']) == 0
+        #Add some test sensors to database
+        assert subprocess.call([sys.executable, os.path.join(PATH, 'teleceptorcmd'), 'loadfixtures']) == 0
+        #start server
+        self.process = subprocess.Popen([sys.executable,os.path.join(PATH,'teleceptorcmd'),'runserver'])
+        time.sleep(2)
+
+    """
+    Set up and teardown before and after each test
+    """
+    def setUp(self):
+        self.startTime = time.time()
+
+    def tearDown(self):
+        t = time.time() - self.startTime
+        print "\n%s: %.3f\n" % (self.id(), t)
+
+    @classmethod
+    def tearDownClass(self):
+        print "Killing Server"
+        self.process.kill()
+        print "Removing tempdir " + DATAPATH
+        shutil.rmtree(DATAPATH)
+
+
+def suite():
+    suite = unittest.TestSuite()
+    suite.addTest(unittest.makeSuite(TestTeleceptor, 'test'))
+    return suite
+
+
+
+if __name__ == "__main__":
+
+    unittest.main()
--- unittests/functionalTest.py
+++ unittests/functionalTest.py
@@ -8,48 +8,11 @@ import json
 import requests
 import tempfile
 import shutil
-PATH = os.path.abspath(os.path.join(os.path.dirname(__file__)))
-sys.path.append(PATH)
 
+#local
+from abstractTest import AbstractTeleceptorTest, URL
 
-DATAPATH = tempfile.mkdtemp(prefix="tmpteleceptor")
-TEST_PORT = 20000
-URL = "http://localhost:%d/" % (TEST_PORT)
-print URL
-print DATAPATH
-
-def build_config():
-
-    data = {"DBFILE": "database/base_station.db",
-            "WHISPER_DATA": "whisperData",
-            "WHISPER_ARCHIVES": ["60:1440", "15m:14d", "6h:1y"],
-            "SQLDATA": True,
-            "SQLREADTIME": 7200,
-            "PORT": TEST_PORT,
-            "LOG": "teleceptor.log",
-            "TCP_POLLER_HOSTS": [],
-            "USE_DEBUG": False,
-            "SUPRESS_SERVER_OUTPUT": True}
-
-    conf = open(os.path.join(DATAPATH, 'config.json'), "w")
-    json.dump(data,conf,indent=4)
-    conf.close()
-
-
-class TestTeleceptor(unittest.TestCase):
-
-    process = None
-
-    @classmethod
-    def setUpClass(self):
-        build_config()
-        os.environ["TELECEPTOR_DATAPATH"] = DATAPATH
-        print sys.executable + " "+ os.path.join(PATH,'teleceptorcmd setup')
-        assert subprocess.call([sys.executable, os.path.join(PATH, 'teleceptorcmd'), 'setup']) == 0
-        assert subprocess.call([sys.executable, os.path.join(PATH, 'teleceptorcmd'), 'loadfixtures']) == 0
-        self.process = subprocess.Popen([sys.executable,os.path.join(PATH,'teleceptorcmd'),'runserver'])
-        time.sleep(2)
-        #insert test sensors
+class TestTeleceptor(AbstractTeleceptorTest):
 
     def test00_base_url(self):
         r = requests.get(URL)
@@ -179,23 +142,6 @@ class TestTeleceptor(unittest.TestCase):
         print responsedata
         self.assertTrue('message_queues' in responsedata)
         self.assertTrue(len(responsedata['message_queues']) > 0)
-
-    """
-    Set up and teardown before and after each test
-    """
-    def setUp(self):
-        self.startTime = time.time()
-
-    def tearDown(self):
-        t = time.time() - self.startTime
-        print "\n%s: %.3f\n" % (self.id(), t)
-
-    @classmethod
-    def tearDownClass(self):
-        print "Killing Server"
-        self.process.kill()
-        print "Removing tempdir " + DATAPATH
-        shutil.rmtree(DATAPATH)
 
 
 def suite():
