940fff6632e83745e2ddd6ec3e87f97cc5f1105c
Cyrille Gindreau <cyrille@visgence.com>
2017-01-27 16:19:14 -0700

    changed countdown to interval to avoid multiple instances of recursion occuring

--- webroot/js/timeController.js
+++ webroot/js/timeController.js
@@ -4,39 +4,46 @@ angular.module('teleceptor.timecontroller', ['ui.bootstrap.datetimepicker'])
 
 .controller('timeController', ['$scope', '$location', 'timeService', '$timeout', function($scope, $location, timeService, $timeout){
 
+    var intervalId;
     $scope.selectTime = function($event, time){
         $($event.target).parent().children().removeClass('active');
         $($event.target).addClass('active');
-        $scope.intervalTimer = 10;
-        switch(time){
-            case 0:
-                $location.search('time', 'custom');
-                setTimes(null, null);
-                $scope.showTimer = false;
-                 refreshing = false;
-                break;
-            case 1:
-                $location.search('time', 'hour');
-                setTimes(Date.now() - 3600000, Date.now());
-                $scope.showTimer = true;
-                refreshing = true;
-                IntervalTimer();
-                break;
-            case 2:
-                $location.search('time', 'day');
-                setTimes(Date.now() - 86400000, Date.now());
-                $scope.showTimer = true;
-                refreshing = true;
-                IntervalTimer();
-                break;
-            case 3:
-                $location.search("time", 'week');
-                setTimes(Date.now() - 604800000, Date.now());
-                $scope.showTimer = true;
-                refreshing = true;
-                IntervalTimer();
-                break;
-        }
+        $scope.toggleAutoRefresh();
+        refreshing = false;
+        $timeout(function(){
+            $scope.intervalTimer = 10;
+            switch(time){
+                case 0:
+                    $location.search('time', 'custom');
+                    setTimes(null, null);
+                    $scope.showTimer = false;
+                    refreshing = false;
+                    clearInterval(intervalId);
+                    break;
+                case 1:
+                    $location.search('time', 'hour');
+                    setTimes(Date.now() - 3600000, Date.now());
+                    $scope.showTimer = true;
+                    refreshing = true;
+                    intervalId = setInterval(IntervalTimer, 1000);
+                    break;
+                case 2:
+                    $location.search('time', 'day');
+                    setTimes(Date.now() - 86400000, Date.now());
+                    $scope.showTimer = true;
+                    refreshing = true;
+                    intervalId = setInterval(IntervalTimer, 1000);
+                    break;
+                case 3:
+                    $location.search("time", 'week');
+                    setTimes(Date.now() - 604800000, Date.now());
+                    $scope.showTimer = true;
+                    refreshing = true;
+                    intervalId = setInterval(IntervalTimer, 1000);
+                    break;
+            }
+        });
+        $scope.toggleAutoRefresh();
     };
 
     $scope.SubmitTime = function(){
@@ -58,46 +65,48 @@ angular.module('teleceptor.timecontroller', ['ui.bootstrap.datetimepicker'])
         var type = $location.search().time;
         setTimes(start, end);
         $scope.showTimer =true;
+        refreshing = true
+        intervalId = setInterval(IntervalTimer, 1000);
         if(type === "custom"){
-            $scope.toggleAutoRefresh();
+            clearInterval(intervalId);
             $scope.showTimer =false;
+            refreshing = false;
+        }
 
+        if(type===undefined){
+            type = "day";
         }
         angular.element("#"+type+"Btn").addClass('active');
-
     }
 
     $scope.intervalTimer = 10;
     var refreshing = true;
 
     function IntervalTimer(){
+        if(refreshing === false) clearInterval(intervalId);
         $timeout(function(){
-            if(refreshing === false) return;
-            $scope.intervalTimer--;
-            if($scope.intervalTimer === 0){
-                $location.search('reload', parseInt(Date.now()/1000));
-                $scope.intervalTimer = 10;
-            }
-            IntervalTimer();
-        }, 1000);
+        $scope.intervalTimer--;
+        if($scope.intervalTimer === 0){
+            $location.search('reload', parseInt(Date.now()/1000));
+            $scope.intervalTimer = 10;
+        }
+    });
     }
 
     $scope.toggleAutoRefresh = function(){
         if(refreshing === true){
-            console.log("turn off")
             $scope.intervalTimer = "";
+            clearInterval(intervalId)
             refreshing = false;
             angular.element("#refreshButton").removeClass("active");
         } else {
-            console.log("turn on")
             $scope.intervalTimer = 10;
             refreshing = true;
-            IntervalTimer();
+            intervalId = setInterval(IntervalTimer, 1000);
             angular.element("#refreshButton").addClass("active");
         }
     };
 
-    IntervalTimer();
     $timeout(function(){
         startUp();
     },100);
