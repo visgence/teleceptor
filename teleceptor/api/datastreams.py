49611914f0538505a529181c08ae638cc4de5b79
Cyrille Gindreau <cyrille@visgence.com>
2017-04-20 14:37:56 -0600

    moved all session management to main GET/POST/PUT/DELETE methods

--- teleceptor/api/datastreams.py
+++ teleceptor/api/datastreams.py
@@ -124,19 +124,16 @@ class DataStreams:
                 'stream':  A single stream
         """
         logging.debug("GET request to datastreams.")
-
         cherrypy.response.headers['Content-Type'] = 'application/json'
         data = {}
         statusCode = "200"
-
-        if stream_id is not None:
-            logging.debug("Request for datastream with id %s", str(stream_id))
-            with sessionScope() as s:
+        with sessionScope() as s:
+            if stream_id is not None:
+                logging.debug("Request for datastream with id %s", str(stream_id))
                 try:
                     stream = s.query(DataStream).filter_by(id=stream_id).one()
                 except NoResultFound:
                     logging.error("Stream with id %s does not exist.", str(stream_id))
-
                     data['error'] = "Stream with id %s doesn't exist." % stream_id
                 else:
                     logging.debug("Found stream with id %s: %s", str(stream_id), str(stream.toDict()))
@@ -149,18 +146,16 @@ class DataStreams:
                         data['path'] = []
                         for i in path:
                             data['path'].append(i.toDict())
-        else:
-            logging.debug("Request for all datastreams with parameters %s", str(filter_arguments))
-            inputs = clean_inputs(filter_arguments)
-            if len(filter_arguments) > 0 and inputs is None:
-                logging.error("Provided url parameters are invalid: %s", str(filter_arguments))
-                data['error'] = "Invalid url parameters"
             else:
-                logging.debug("Parameters are valid.")
-                with sessionScope() as s:
+                logging.debug("Request for all datastreams with parameters %s", str(filter_arguments))
+                inputs = clean_inputs(filter_arguments)
+                if len(filter_arguments) > 0 and inputs is None:
+                    logging.error("Provided url parameters are invalid: %s", str(filter_arguments))
+                    data['error'] = "Invalid url parameters"
+                else:
+                    logging.debug("Parameters are valid.")
                     datastreams = s.query(DataStream).filter_by(**inputs).all()
                     data['datastreams'] = [i.toDict() for i in datastreams]
-
         cherrypy.response.status = statusCode
         return json.dumps(data, indent=4)
 
@@ -196,14 +191,15 @@ class DataStreams:
 
         logging.debug("Request body: %s", data)
 
-        try:
-            stream = DataStreams.updateStream(stream_id, data)
-            returnData['stream'] = stream
-        except NoResultFound:
-            logging.error("Stream with id %s doesn't exist.", str(stream_id))
-            returnData['error'] = "Stream with id %s doesn't exist." % stream_id
+        with sessionScope() as session:
+            try:
+                stream = DataStreams.updateStream(stream_id, data, session)
+                returnData['stream'] = stream
+            except NoResultFound:
+                logging.error("Stream with id %s doesn't exist.", str(stream_id))
+                returnData['error'] = "Stream with id %s doesn't exist." % stream_id
 
-        cherrypy.response.status = statusCode
+            cherrypy.response.status = statusCode
 
         logging.debug("Finished PUT request to datastream.")
         return json.dumps(returnData, indent=4)
@@ -225,12 +221,9 @@ class DataStreams:
             `models.DataStream`
         """
         logging.debug("DELETE request to datastreams for stream with id {}".format(stream_id))
-
         cherrypy.response.headers['Content-Type'] = 'application/json'
         data = {}
-
         statusCode = "200"
-
         with sessionScope() as s:
             try:
                 deletedStream = deleteDatastream(s, stream_id)
@@ -270,7 +263,7 @@ class DataStreams:
             logging.error("Provided datastream to createDatastream method is None.")
 
     @staticmethod
-    def updateStream(stream_id, data, session=None):
+    def updateStream(stream_id, data, session):
         """
         Updates stream with id `stream_id` with new key/values in `data`. Note that this function will incur a db lookup.
 
@@ -373,28 +366,21 @@ def clean_inputs(inputs):
             :returns:
                 Dictionary with valid parameters taken from provided inputs.
         """
-
         validInputs = {
             'sensor': '^[a-zA-Z0-9_.]+$'
         }
-
         valueConversions = {
             'null': None
         }
-
         safeInputs = {}
-
         for key, value in inputs.iteritems():
-            if not key in validInputs:
+            if key not in validInputs:
                 return None
-
             if value in valueConversions:
                 value = valueConversions[value]
             elif re.match(validInputs[key], value) is None:
                 return None
-
             safeInputs[key] = value
-
         return safeInputs
 
 
@@ -413,12 +399,11 @@ def get_datastream(datastream_id, session=None):
         noResultFound: If no datastream matches the datastream_id.
     """
     if session is None:
-        with sessionScope() as session:
-            stream = session.query(DataStream).filter_by(id=datastream_id).one()
-            return stream.toDict()
-    else:
-        stream = session.query(DataStream).filter_by(id=datastream_id).one()
-        return stream.toDict()
+        with sessionScope() as s:
+            session = s
+
+    stream = session.query(DataStream).filter_by(id=datastream_id).one()
+    return stream.toDict()
 
 
 def get_datastream_by_sensorid(sensor_id, session=None):
@@ -436,12 +421,10 @@ def get_datastream_by_sensorid(sensor_id, session=None):
         NoResultFound: If no datastream matches the datastream_id.
     """
     if session is None:
-        with sessionScope() as session:
-            stream = session.query(DataStream).filter_by(sensor=sensor_id).one()
-            return stream.toDict()
-    else:
-        stream = session.query(DataStream).filter_by(sensor=sensor_id).one()
-        return stream.toDict()
+        with sessionScope() as s:
+            session = s
+    stream = session.query(DataStream).filter_by(sensor=sensor_id).one()
+    return stream.toDict()
 
 
 def filter_datastreams(session=None, **filter_args):
@@ -456,9 +439,7 @@ def filter_datastreams(session=None, **filter_args):
     """
     inputs = clean_inputs(filter_args)
     if session is None:
-        with sessionScope() as session:
-            streams = session.query(DataStream).filter_by(inputs).all()
-            return [stream.toDict() for stream in streams]
-    else:
-        streams = session.query(DataStream).filter_by(inputs).all()
-        return [stream.toDict() for stream in streams]
+        with sessionScope() as s:
+            session = s
+    streams = session.query(DataStream).filter_by(inputs).all()
+    return [stream.toDict() for stream in streams]
--- teleceptor/api/readings.py
+++ teleceptor/api/readings.py
@@ -40,12 +40,10 @@ if USE_ELASTICSEARCH:
 
 class SensorReadings:
     exposed = True
-
     if USE_DEBUG:
         logging.basicConfig(format='%(levelname)s:%(asctime)s %(message)s', level=logging.DEBUG)
     else:
         logging.basicConfig(format='%(levelname)s:%(asctime)s %(message)s', level=logging.INFO)
-
     validFilterArgs = {
         'datastream': '^\d+$',
         'start':      '^\d+$',
@@ -53,7 +51,6 @@ class SensorReadings:
         'points':     '^\d+$',
         'source':    '^SQL|ElasticSearch$'
     }
-
     validOperatorArgs = {
         'condense':    '^true|false$',
         'granularity': '^\d+$'
@@ -86,16 +83,12 @@ class SensorReadings:
             :returns: Dictionary -- Dict with clean parameters and None if any parameter was unrecognized or the parameter
                 did not have a correct value format.
         """
-
         logging.debug("Validating parameters: %s", str(params))
-
         valueConversions = {
             'null': None
         }
-
         safeParams = {}
         paramsCopy = params.copy()
-
         for key, value in paramsCopy.iteritems():
 
             if key in self.validFilterArgs:
@@ -230,15 +223,14 @@ class SensorReadings:
         data = {}
         inputs = self.cleanInputs(kwargs)
         logging.debug("Got clean input arguments %s", str(inputs))
-
         if len(kwargs) > 0 and inputs is None:
             logging.error("Got invalid url parameters %s", str(kwargs))
             data['error'] = "Invalid url parameters"
             status_code = "400"
         else:
-            with sessionScope() as s:
+            with sessionScope() as session:
                 try:
-                    data['readings'], data['source'] = self.filterReadings(s, inputs)
+                    data['readings'], data['source'] = self.filterReadings(session, inputs)
                 except ValueError as e:
                     data['error'] = str(e)
 
@@ -270,11 +262,12 @@ class SensorReadings:
 
         logging.debug("Request body: %s", str(reading_data))
 
-        try:
-            data = insertReadings(reading_data['readings'])
-        except KeyError:
-            logging.error("No readings in request body to insert.")
-            data['error'] = "No readings to insert"
+        with sessionScope() as session:
+            try:
+                data = insertReadings(reading_data['readings'], session)
+            except KeyError:
+                logging.error("No readings in request body to insert.")
+                data['error'] = "No readings to insert"
 
         cherrypy.response.status = status_code
         logging.info("Finished POST request to readings.")
@@ -294,42 +287,24 @@ class SensorReadings:
         statusCode = "200"
 
         if datastream_id is not None:
-            try:
-                deleted_readings = deleteReadingsByDatastream(datastream_id)
-                data['readings'] = deleted_readings
-            except Exception as e:
-                error_string = "Unexpected error while deleting readings by datastream: {}".format(e)
-                logging.exception(error_string)
-                logging.info(error_string)
-                data['error'] = error_string
-                statusCode = "400"
+            with sessionScope() as session:
+                try:
+                    deleted_readings = deleteReadingsByDatastream(datastream_id, session)
+                    data['readings'] = deleted_readings
+                except Exception as e:
+                    error_string = "Unexpected error while deleting readings by datastream: {}".format(e)
+                    logging.exception(error_string)
+                    logging.info(error_string)
+                    data['error'] = error_string
+                    statusCode = "400"
 
         cherrypy.response.status = statusCode
         logging.info("Finished DELETE request to datastreams.")
         return json.dumps(data, indent=4)
 
 
-def insertReadings(readings, session=None):
-    """
-    Tries to insert the readings provided into database, and optionally into the SQL database if SQLDATA is set.
+def insertReadings(readings, session):
 
-    :param readings: List of reading tuples of the form (datastreamid, value, timestamp)
-
-    :returns: Dictionary -- A dict with keywords "insertions_attempted", "successfull_insertions", and "failed_insertions"
-    """
-
-    logging.debug("Inserting readings.")
-    if session is None:
-        with sessionScope() as session:
-            data = _insertReadings(readings, session)
-    else:
-        data = _insertReadings(readings, session)
-
-    logging.debug("Stats from attempted insertions: %s", str(data))
-    return data
-
-
-def _insertReadings(readings, session=None):
     """
     Tries to insert the readings provided into database, and optionally into the SQL database if SQLDATA is set.
 
@@ -346,15 +321,12 @@ def _insertReadings(readings, session=None):
         "successfull_insertions": 0,
         "failed_insertions": 0
     }
-
     DS = 0
     VAL = 1
     TIME = 2
-
     for reading in readings:
         logging.debug("Looking at reading %s", str(reading))
         data['insertions_attempted'] += 1
-
         try:
             streamId = reading[DS]
             rawVal = reading[VAL]
@@ -362,12 +334,10 @@ def _insertReadings(readings, session=None):
         except:
             logging.error("Error separating %s into streamId, rawVal, and timestamp.", str(reading))
             continue
-
         # If no sensor value then skip this reading
         if rawVal is None or rawVal == "":
             logging.error("Provided rawVal is invalid: %s", str(rawVal))
             continue
-
         # TODO: handle errors better
         # Get the datastream, if possible
         try:
@@ -377,7 +347,6 @@ def _insertReadings(readings, session=None):
         except NoResultFound:
             logging.error("No datastream with id %s exists.", str(streamId))
             continue
-
         try:
             logging.debug("Inserting into database with streamId %s, rawVal %s, and timestamp %s", str(streamId), str(rawVal), str(timestamp))
             if USE_ELASTICSEARCH:
@@ -402,38 +371,13 @@ def _insertReadings(readings, session=None):
     return data
 
 
-def deleteReadingsByDatastream(datastream_id, session=None):
+def deleteReadingsByDatastream(datastream_id, session):
     """
     Deletes all readings with the datastream id `datastream_id`.
-
     :param datastream_id: id of the datastream to delete all of the readings for
     :type datastream_id: int
     :param session: Existing context into a sqlalchemy database session. Can be created by a call to `sessionScope()`
     :type session: context object from `sessionScope()`
-
-    :returns: A list of the deleted readings
-
-    .. seealso::
-        `models.DataStream`
-        `models.SensorReading`
-        `readings.DELETE`
-    """
-
-    if session is None:
-        with sessionScope() as s:
-            return _deleteReadingsByDatastream(datastream_id, s)
-    return _deleteReadingsByDatastream(datastream_id, session)
-
-
-def _deleteReadingsByDatastream(datastream_id, session):
-    """
-    Deletes all readings with the datastream id `datastream_id`.
-
-    :param datastream_id: id of the datastream to delete all of the readings for
-    :type datastream_id: int
-    :param session: Existing context into a sqlalchemy database session. Can be created by a call to `sessionScope()`
-    :type session: context object from `sessionScope()`
-
     :returns: A list of the deleted readings
 
     .. seealso::
